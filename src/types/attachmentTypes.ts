dd31b691df28761e3e454f3c648fc298af348cff
Tom Laird-McConnell <tomlm@microsoft.com>
2016-10-26 18:02:17 -0700

    initial implementation of attachment api

--- src/types/attachmentTypes.ts
+++ src/types/attachmentTypes.ts
@@ -7,3 +7,38 @@ export interface IAttachment {
     name?: string,
     thumbnailUrl?: string,
 }
+
+
+export interface IAttachmentData {
+    /// content type of the attachmnet
+    type: string;
+
+    /// Name of the attachment
+    name: string;
+
+    /// original content
+    originalBase64: string;
+
+    /// Thumbnail 
+    thumbnailBase64: string;
+}
+
+export interface IAttachmentInfo {
+
+    /// Name of the attachment
+    name: string;
+
+    /// ContentType of the attachment
+    type: string;
+
+    /// attachment views 
+    views: IAttachmentView[];
+}
+
+export interface IAttachmentView {
+    /// content type of the attachmnet
+    viewId: string;
+
+    /// Name of the attachment
+    size: number;
+}
