12f11e9977e70015910d8af62fb8ce751478a708
Tom Laird-McConnell <tomlm@microsoft.com>
2016-10-28 11:03:36 -0700

    cleaned up some imports

--- src/server/framework/frameworkServer.ts
+++ src/server/framework/frameworkServer.ts
@@ -5,10 +5,9 @@ import { AttachmentsController } from './attachmentsController';
 import { BotStateController } from './botStateController';
 import { RestServer } from '../restServer';
 import { getStore, getSettings } from '../settings';
-var platform = require('os').platform();
-var ngrok = require('ngrok'); // import * as ngrok from 'ngrok';
 import * as Fs from 'fs';
-
+import * as Os from 'os';
+var ngrok = require('ngrok'); // import * as ngrok from 'ngrok';
 
 /**
  * Communicates with the bot.
@@ -36,6 +35,14 @@ export class FrameworkServer extends RestServer {
      */
     private configure() {
         const settings = getSettings();
+        // copy ngrok to target path
+        // var targetNgrokPath = __dirname + '/bin/ngrok' + (Os.platform()  === 'win32' ? '.exe' : '');
+        // if (!Fs.existsSync(__dirname + "/bin"))
+        //     Fs.mkdirSync(__dirname + "/bin");
+
+        // if (!Fs.existsSync(targetNgrokPath) && settings.framework.ngrokPath)
+        //     Fs.linkSync(settings.framework.ngrokPath, targetNgrokPath);
+
         if (this.port !== settings.framework.port) {
             console.log(`restarting ${this.server.name} because ${this.port} !== ${settings.framework.port}`);
             this.restart(settings.framework.port);
