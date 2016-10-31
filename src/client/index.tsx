9e9016ea79e23e504c8e6c7f19f852760e191912
Eric Anderson <eanders@microsoft.com>
2016-10-30 22:33:07 -0700

    install hyperlink handler

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,10 +3,22 @@ import * as ReactDOM from 'react-dom';
 import { MainView } from './mainView';
 import * as Settings from './settings';
 import { uniqueId } from '../utils';
+import { navigate } from './hyperlinkHandler';
 
+
+const interceptClickEvent = (e: any) => {
+    let target = e.target;
+    while (target) {
+        if (target.tagName === 'A') {
+            navigate(target.href);
+            return;
+        }
+        target = target.parentNode;
+    }
+}
+
+document.addEventListener('click', interceptClickEvent);
 
 Settings.startup();
 
-ReactDOM.render(
-    <MainView />,
-    document.getElementById('mainview'));
+ReactDOM.render(<MainView />, document.getElementById('mainview'));
