c94de8d1dde79a2dac25f98ff8528bed9a371a58
Eric Anderson <eanders@microsoft.com>
2016-11-10 00:12:04 -0800

    restore baseline logging option, for A/B testing.

--- src/client/logView.tsx
+++ src/client/logView.tsx
@@ -6,6 +6,11 @@ import { getSettings, addSettingsListener } from './settings';
 import { LogActions } from './reducers';
 
 
+// TEMPORARY, for A/B testing logview layout
+// If you change this, also change it in server/log.ts
+const useTables = false;
+
+
 export enum Severity {
     log,
     info,
@@ -45,21 +50,32 @@ const timestamp = (entry: ILogEntry) => {
     const hours = number2(entry.timestamp.getHours());
     const minutes = number2(entry.timestamp.getMinutes());
     const seconds = number2(entry.timestamp.getSeconds());
-    return <td className='wc-logview-timestamp'>{`[${hours}:${minutes}:${seconds}]`}</td>
+    if (useTables) {
+        return <td className='wc-logview-timestamp-tables'>{`[${hours}:${minutes}:${seconds}]`}</td>
+    } else {
+        return <div className='wc-logview-timestamp'>{`[${hours}:${minutes}:${seconds}]`}&nbsp;</div>
+    }
 }
 
 const emit = (val: any, className: string, colspan?: number) => {
-    if (!colspan)
-        colspan = 1;
-
     if (!val) return null;
-    if (val.hasOwnProperty('messageType') && val['messageType'] === 'link') {
-        if (val.link.startsWith('emulator'))
-            return <td className={className} colSpan={colspan}><a className='wc-logview-link' href={val.link}>{val.text}</a></td>
-        else
-            return <td className={className} colSpan={colspan}><a className='wc-logview-link' title={val.link} href={val.link}>{val.text}</a></td>
+    if (useTables) {
+        if (!colspan)
+            colspan = 1;
+        if (val.hasOwnProperty('messageType') && val['messageType'] === 'link') {
+            if (val.link.startsWith('emulator'))
+                return <td className={className} colSpan={colspan}><a className='wc-logview-link' href={val.link}>{val.text}</a></td>
+            else
+                return <td className={className} colSpan={colspan}><a className='wc-logview-link' title={val.link} href={val.link}>{val.text}</a></td>
+        } else {
+            return <td className={className} colSpan={colspan}>{safeStringify(val)}</td>
+        }
     } else {
-        return <td className={className} colSpan={colspan}>{safeStringify(val)}</td>
+        if (val.hasOwnProperty('messageType') && val['messageType'] === 'link') {
+            return <div className={className}><a className='wc-logview-link' href={val.link}>{val.text}</a>&nbsp;</div>
+        } else {
+            return <div className={className}>{safeStringify(val)}&nbsp;</div>
+        }
     }
 }
 
@@ -67,23 +83,33 @@ const message = (entry: ILogEntry, className: string) => {
     return emit(entry.message, className, 4 - entry.args.length);
 }
 
-const args = (entry: ILogEntry, className: string) => {
+const args = (entry: ILogEntry, className?: string) => {
     if (entry.args && entry.args.length) {
         return entry.args
             .filter(arg => !!arg)
-            .map((arg, i) => emit(arg, className));
+            .map((arg, i) => emit(arg, useTables ? className : 'wc-logview-arg'));
     }
     return null;
 }
 
 const format = (entry: ILogEntry, index: number, items: any[]) => {
-    return (
-        <tr className='emu-log-entry'>
-            {timestamp(entry)}
-            {message(entry, 'wc-logview-' + Severity[entry.severity] + ' wc-logview-message')}
-            {args(entry, 'wc-logview-' + Severity[entry.severity])}
-        </tr>
-    );
+    if (useTables) {
+        return (
+            <tr className='emu-log-entry'>
+                {timestamp(entry)}
+                {message(entry, 'wc-logview-' + Severity[entry.severity] + '-tables' + ' wc-logview-message-tables')}
+                {args(entry, 'wc-logview-' + Severity[entry.severity] + '-tables')}
+            </tr>
+        );
+    } else {
+        return (
+            <div className='emu-log-entry'>
+                {timestamp(entry)}
+                {message(entry, 'wc-logview-' + Severity[entry.severity])}
+                {args(entry)}
+            </div>
+        );
+    }
 }
 
 
@@ -142,13 +168,21 @@ export class LogView extends React.Component<{}, ILogViewState> {
     }
 
     render() {
-        return (
-            <div className="wc-logview" ref={ref => this.scrollMe = ref}>
-                <table className="wc-logview-table">
+        if (useTables) {
+            return (
+                <div className="wc-logview-tables" ref={ref => this.scrollMe = ref}>
+                    <table className="wc-logview-table">
+                        {this.state.entries.map((entry, i, items) => format(entry, i, items))}
+                    </table>
+                </div>
+            );
+        } else {
+            return (
+                <div className="wc-logview" ref={ref => this.scrollMe = ref}>
                     {this.state.entries.map((entry, i, items) => format(entry, i, items))}
-                </table>
-            </div>
-        );
+                </div>
+            );
+        }
     }
 
     public static add(severity: Severity, message: any, ...args: any[]) {
