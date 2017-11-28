fa5f143097a98602e3abdf08172a081aa211d970
Nils Whitmont <v-niwh@microsoft.com>
2017-11-28 14:11:13 -0800

    split messages linked/not linked

--- src/server/controllers/connector/botStateController.ts
+++ src/server/controllers/connector/botStateController.ts
@@ -65,7 +65,8 @@ export class BotStateController {
         } else {
             if (!conversation.stateApiDeprecationWarningShown) {
                 conversation.stateApiDeprecationWarningShown = true;
-                log.warn('Warning: The Bot Framework State API is not recommended for production environments, and may be deprecated in a future release.' + log.makeLinkMessage('Learn how to implement your own storage adapter.', 'https://aka.ms/botframework-state-service'));
+                log.warn('Warning: The Bot Framework State API is not recommended for production environments, and may be deprecated in a future release.');
+                log.makeLinkMessage('Learn how to implement your own storage adapter.', 'https://aka.ms/botframework-state-service')
             }
         }
         
