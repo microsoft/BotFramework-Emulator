c18c9e173c100b833781a448991ee794667d8ef4
Mike Bennett <mgbennet@gmail.com>
2017-12-20 15:26:49 -0800

    Added test for payment window to integration test. (#375)

--- tests/testBot/testBot.ts
+++ tests/testBot/testBot.ts
@@ -75,3 +75,109 @@ mainDialog.onDefault(function(session, args, next) {
     session.send("%s, you said %s", getCounter(session), session.message.text);
 });
 bot.dialog('/', mainDialog);
+
+//
+// Payments
+//
+mainDialog.matches(/\bpayment\b/i, function(session, args, next) {
+    let buyCard = new builder.HeroCard(session);
+    buyCard.title("Product card")
+        .text("Press button to open payment window.")
+        .buttons([
+            {
+                "title": "Buy",
+                "type": "payment",
+                "value": paymentRequest
+            }
+        ]);
+    let message = new builder.Message(session);
+    session.send(message.addAttachment(buyCard));
+});
+connector.onInvoke((invoke, callback) => {
+    let storageCtx = {
+        address: invoke.relatesTo,
+        persistConversationData: true,
+        conversationId: invoke.relatesTo.conversation.id
+    };
+  
+    connector.getData(storageCtx, (err, data) => {
+        switch (invoke.name) {
+            case 'payments/update/shippingAddress':
+            case 'payments/update/shippingOption':
+                console.log("updatedshipping info");
+                callback(null, {}, 200);
+                break;
+
+            case 'payments/complete':
+                console.log("completed payment request");
+                callback(null, {result: "success"}, 200);
+                bot.beginDialog(invoke.relatesTo, 'checkout_receipt');
+                break;
+        }
+    });
+});
+
+bot.dialog('checkout_receipt', function (session, args) {
+    session.endDialog("Completed payment");
+});
+
+bot.dialog('checkout_failed', function (session, args) {
+    session.endDialog('Could not process your payment: %s', args.errorMessage);
+});
+
+var paymentRequest = {
+    id: "cartId",
+    expires: '1.00:00:00',
+    methodData: [{
+        supportedMethods: ['https://pay.microsoft.com/microsoftpay'],
+        data: {
+            mode: 'TEST',
+            merchantId: '12345678-abc1-23d4-5ef6-78a9b0c12d3d',
+            supportedNetworks: ['visa', 'mastercard'],
+            supportedTypes: ['credit']
+        }
+    }],
+    details: {
+        total: {
+            label: 'Total',
+            amount: { currency: 'USD', value: 1.99 },
+            pending: true
+        },
+        displayItems: [
+        {
+            label: "Doodad",
+            amount: { currency: 'USD', value: 1.99 }
+        }, {
+            label: 'Shipping',
+            amount: { currency: 'USD', value: '0.00' },
+            pending: true
+        }, {
+            label: 'Sales Tax',
+            amount: { currency: 'USD', value: '0.00' },
+            pending: true
+        }],
+        shippingOptions: [
+        {
+            "id": "GROUND",
+            "label": "Ground",
+            "amount": {
+                "currency": "USD",
+                "value": "8.95"
+            }
+        }, {
+            "id": "AIR",
+            "label": "Air",
+            "amount": {
+                "currency": "USD",
+                "value": "24.99"
+            }
+        }]
+    },
+    options: {
+        requestPayerName: true,
+        requestPayerEmail: true,
+        requestPayerPhone: true,
+        requestShipping: true,
+        shippingType: 'GROUND'
+    }
+}
