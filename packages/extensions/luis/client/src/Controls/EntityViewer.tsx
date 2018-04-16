538c887017080810e029f9b43b4f845420a9a726
Emad Elwany <Emad.Elwany@microsoft.com>
2018-04-16 23:32:53 +0000

    Merged PR 4657: Improve rendering of entity values
    Improve rendering of entity values
    
    Related work items: #21805

--- packages/extensions/luis/client/src/Controls/EntityViewer.tsx
+++ packages/extensions/luis/client/src/Controls/EntityViewer.tsx
@@ -1,5 +1,27 @@
 import * as React from 'react';
 import { Component } from 'react';
+import { css } from 'glamor';
+
+const CSS = css({
+  display: 'block',
+
+  '& #entityName': {
+    display: 'inline-block',
+    overflow: 'hidden'
+  },
+
+  '& #arrow': {
+    display: 'inline-block',
+    overflow: 'hidden'
+  },
+
+  '& #entityValue': {
+    display: 'inline-block',
+    maxWidth: '150px',
+    textOverflow: 'ellipsis',
+    overflow: 'hidden'
+  }
+});
 
 interface EntityInfo {
   name: string;
@@ -18,10 +40,26 @@ class EntityViewer extends Component<EntityViwerProps, EntityViewerState> {
 
   static renderEntityValue(entityValue: any): string {
     if (Array.isArray(entityValue)) {
+      entityValue = EntityViewer.flattenEntityValue(entityValue);
+    }
+
+    if (Array.isArray(entityValue)) {
+      if ( typeof entityValue[0] === 'object') {
+        entityValue = entityValue.map(ev => JSON.stringify(ev));
+      }
       return entityValue.join(', ');
     }
 
+    if ( typeof entityValue === 'object' ) {
+      return JSON.stringify(entityValue);
+    }
+
+    // primitive
     return entityValue;
+  }
+
+  static flattenEntityValue(entityValueArr: any[]) {
+    return [].concat.apply([], entityValueArr);
   }
 
   constructor(props: any, context: any) {
@@ -31,10 +69,10 @@ class EntityViewer extends Component<EntityViwerProps, EntityViewerState> {
 
   render() {
     return (
-      <div>
-        <span>{this.props.entity.name}</span>
-        <span> --> </span>
-        <span>{EntityViewer.renderEntityValue(this.props.entity.value)}</span>
+      <div {...CSS}>
+        <div id="entityName">{this.props.entity.name}</div>
+        <div id="arrow">&nbsp; -->&nbsp; </div>
+        <div id="entityValue">{EntityViewer.renderEntityValue(this.props.entity.value)}</div>
       </div>
     );
   }
