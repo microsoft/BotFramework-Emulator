360e0542b2b8ae36c92f2fcc6d2b9018dc763c14
William Wong <Hak.Wong@microsoft.com>
2018-04-18 15:40:14 +0000

    Merged PR 4677: Better dirty algorithm
    Enable un-"dirty" by reverting changes manually

--- packages/app/client/src/ui/editor/appSettingsEditor.tsx
+++ packages/app/client/src/ui/editor/appSettingsEditor.tsx
@@ -51,7 +51,28 @@ interface IAppSettingsEditorProps {
   dirty?: boolean;
 }
 
-export default class AppSettingsEditor extends React.Component<IAppSettingsEditorProps, IFrameworkSettings> {
+interface IAppSettingsEditorState {
+  committed: IFrameworkSettings;
+  uncommitted: IFrameworkSettings;
+};
+
+const defaultAppSettings: IFrameworkSettings = {
+  bypassNgrokLocalhost: true,
+  locale: '',
+  localhost: '',
+  ngrokPath: '',
+  stateSizeLimit: 64,
+  use10Tokens: false
+}
+
+function shallowEqual(x, y) {
+  return (
+    Object.keys(x).length === Object.keys(y).length
+    && Object.keys(x).every(key => key in y && x[key] === y[key])
+  );
+}
+
+export default class AppSettingsEditor extends React.Component<IAppSettingsEditorProps, IAppSettingsEditorState> {
   constructor(props: IAppSettingsEditorProps, context: any) {
     super(props, context);
 
@@ -61,18 +82,14 @@ export default class AppSettingsEditor extends React.Component<IAppSettingsEdito
     this.onChangeNgrok = this.onChangeNgrok.bind(this);
     this.onChangeNgrokBypass = this.onChangeNgrokBypass.bind(this);
     this.onChangeAuthTokenVersion = this.onChangeAuthTokenVersion.bind(this);
-    this.setDirtyFlag = debounce(this.setDirtyFlag, 500);
+    this.setDirtyFlag = debounce(this.setDirtyFlag, 300);
     this.onClickDiscard = this.onClickDiscard.bind(this);
     this.onChangeLocalhost = this.onChangeLocalhost.bind(this);
     this.onChangeLocale = this.onChangeLocale.bind(this);
 
     this.state = {
-      ngrokPath: '',
-      bypassNgrokLocalhost: true,
-      stateSizeLimit: 64,
-      use10Tokens: false,
-      localhost: '',
-      locale: ''
+      committed: { ...defaultAppSettings },
+      uncommitted: { ...defaultAppSettings }
     };
   }
 
@@ -80,9 +97,41 @@ export default class AppSettingsEditor extends React.Component<IAppSettingsEdito
     // load settings from main and populate form
     CommandService.remoteCall('app:settings:load')
       .then(settings => {
-        this.setState(prevState => ({ ...prevState, ...settings }));
+        this.setState(() => ({
+          committed: settings,
+          uncommitted: settings
+        }));
       })
       .catch(err => console.error('Error while loading app settings: ', err));
+  }
+
+  setUncommittedState(patch) {
+    this.setState(state => {
+      const nextUncommitted = {
+        ...state.uncommitted,
+        ...patch
+      };
+
+      const clean = shallowEqual(state.uncommitted, state.committed);
+      const nextClean = shallowEqual(nextUncommitted, state.committed);
+
+      if (nextClean !== clean) {
+        this.setDirtyFlag(!nextClean);
+      }
+
+      return { uncommitted: nextUncommitted };
+    });
+  }
+
+  commit(committed) {
+    this.setState(state => {
+      this.setDirtyFlag(false);
+
+      return {
+        committed: { ...committed },
+        uncommitted: { ...committed }
+      };
+    });
   }
 
   onClickBrowse(e): void {
@@ -93,58 +142,48 @@ export default class AppSettingsEditor extends React.Component<IAppSettingsEdito
     };
 
     CommandService.remoteCall('shell:showOpenDialog', dialogOptions)
-      .then(path => {
-        this.setState(({ ngrokPath: path }));
-        this.setDirtyFlag(true);
-      })
+      .then(ngrokPath => this.setUncommittedState({ ngrokPath }))
       .catch(err => console.log('User cancelled browsing for ngrok: ', err));
   }
 
   onChangeSizeLimit(e): void {
-    this.setState(({ stateSizeLimit: e.target.value }));
-    this.setDirtyFlag(true);
+    this.setUncommittedState({ stateSizeLimit: e.target.value });
   }
 
   onClickSave(e): void {
+    const { uncommitted } = this.state;
     const settings: IFrameworkSettings = {
-      ngrokPath: this.state.ngrokPath.trim(),
-      bypassNgrokLocalhost: this.state.bypassNgrokLocalhost,
-      stateSizeLimit: +this.state.stateSizeLimit,
-      use10Tokens: this.state.use10Tokens,
-      localhost: this.state.localhost.trim(),
-      locale: this.state.locale.trim()
+      ngrokPath: uncommitted.ngrokPath.trim(),
+      bypassNgrokLocalhost: uncommitted.bypassNgrokLocalhost,
+      stateSizeLimit: +uncommitted.stateSizeLimit,
+      use10Tokens: uncommitted.use10Tokens,
+      localhost: uncommitted.localhost.trim(),
+      locale: uncommitted.locale.trim()
     };
 
     CommandService.remoteCall('app:settings:save', settings)
-      .then(() => {
-        this.setDirtyFlag(false);
-      })
+      .then(() => this.commit(settings))
       .catch(err => console.error('Error while saving app settings: ', err));
   }
 
   onChangeAuthTokenVersion(e): void {
-    this.setState(({ use10Tokens: !this.state.use10Tokens }));
-    this.setDirtyFlag(true);
+    this.setUncommittedState({ use10Tokens: !this.state.uncommitted.use10Tokens });
   }
 
   onChangeNgrok(e): void {
-    this.setState(({ ngrokPath: e.target.value }));
-    this.setDirtyFlag(true);
+    this.setUncommittedState({ ngrokPath: e.target.value });
   }
 
   onChangeNgrokBypass(e): void {
-    this.setState(({ bypassNgrokLocalhost: !this.state.bypassNgrokLocalhost }));
-    this.setDirtyFlag(true);
+    this.setUncommittedState({ bypassNgrokLocalhost: !this.state.uncommitted.bypassNgrokLocalhost });
   }
 
   onChangeLocalhost(e): void {
-    this.setState({ localhost: e.target.value });
-    this.setDirtyFlag(true);
+    this.setUncommittedState({ localhost: e.target.value });
   }
 
   onChangeLocale(e): void {
-    this.setState({ locale: e.target.value });
-    this.setDirtyFlag(true);
+    this.setUncommittedState({ locale: e.target.value });
   }
 
   setDirtyFlag(dirty): void {
@@ -156,6 +195,9 @@ export default class AppSettingsEditor extends React.Component<IAppSettingsEdito
   }
 
   render(): JSX.Element {
+    const { uncommitted } = this.state;
+    const clean = shallowEqual(this.state.committed, uncommitted);
+
     return (
       <GenericDocument style={ CSS }>
         <MediumHeader>App settings</MediumHeader>
@@ -164,30 +206,30 @@ export default class AppSettingsEditor extends React.Component<IAppSettingsEdito
             <SmallHeader>Service settings</SmallHeader>
             <p><a href="https://ngrok.com/" target="_blank">ngrok</a> is network tunneling software. The Bot Framework Emulator works with ngrok to communicate with bots hosted remotely. Read the <a href="https://github.com/Microsoft/BotFramework-Emulator/wiki/Tunneling-(ngrok)" target="_blank">wiki page</a> to learn more about using ngrok and to download it.</p>
             <Row align={ RowAlignment.Center }>
-              <TextInputField readOnly={ false } value={ this.state.ngrokPath } onChange={ this.onChangeNgrok } label={ 'Path to ngrok' } />
-              <PrimaryButton onClick={ this.onClickBrowse } text={ 'Browse' } className="browse-button" />
+              <TextInputField readOnly={ false } value={ uncommitted.ngrokPath } onChange={ this.onChangeNgrok } label={ 'Path to ngrok' } />
+              <PrimaryButton onClick={ this.onClickBrowse } text="Browse" className="browse-button" />
             </Row>
-            <Checkbox checked={ this.state.bypassNgrokLocalhost } onChange={ this.onChangeNgrokBypass } id={ 'ngrok-bypass' } label={ 'Bypass ngrok for local addresses' } />
-            <Checkbox checked={ this.state.use10Tokens } onChange={ this.onChangeAuthTokenVersion } id={ 'auth-token-version' } label={ 'Use version 1.0 authentication tokens' } />
+            <Checkbox checked={ uncommitted.bypassNgrokLocalhost } onChange={ this.onChangeNgrokBypass } id="ngrok-bypass" label="Bypass ngrok for local addresses" />
+            <Checkbox checked={ uncommitted.use10Tokens } onChange={ this.onChangeAuthTokenVersion } id="auth-token-version" label="Use version 1.0 authentication tokens" />
             <Row align={ RowAlignment.Center }>
-              <TextInputField readOnly={ false } value={ this.state.localhost } onChange={ this.onChangeLocalhost } label={ 'localhost override' } />
+              <TextInputField readOnly={ false } value={ uncommitted.localhost } onChange={ this.onChangeLocalhost } label="localhost override" />
             </Row>
             <Row align={ RowAlignment.Center }>
-              <TextInputField readOnly={ false } value={ this.state.locale } onChange={ this.onChangeLocale } label={ 'Locale' } />
+              <TextInputField readOnly={ false } value={ uncommitted.locale } onChange={ this.onChangeLocale } label="Locale" />
             </Row>
           </Column>
           <Column className="right-column">
             <SmallHeader>Bot state settings</SmallHeader>
-            <p>Bots use the <a href="https://docs.microsoft.com/en-us/bot-framework/dotnet/bot-builder-dotnet-state" target="_blank">Bot State service</a> to store and retrieve application data. The Bot Framework's bot state service has a size limit of 64KB. Custom state services may differ.</p>
+            <p>Bots use the <a href="https://docs.microsoft.com/en-us/bot-framework/dotnet/bot-builder-dotnet-state" target="_blank">Bot State service</a> to store and retrieve application data. The Bot Framework's bot state service has a size limit of 64 KB. Custom state services may differ.</p>
             <Row align={ RowAlignment.Center }>
-              <NumberInputField min={ 0 } value={ this.state.stateSizeLimit } onChange={ this.onChangeSizeLimit } label={ 'Size limit (zero for no limit)' } />
+              <NumberInputField min={ 0 } value={ uncommitted.stateSizeLimit } onChange={ this.onChangeSizeLimit } label="Size limit (zero for no limit)" />
               <span className="size-limit-suffix">KB</span>
             </Row>
           </Column>
         </Row>
         <Row className="button-row" justify={ RowJustification.Right }>
-          <PrimaryButton secondary text='Cancel' onClick={ this.onClickDiscard } />
-          <PrimaryButton text='Save' onClick={ this.onClickSave } className="save-button" disabled={ !this.props.dirty } />
+          <PrimaryButton secondary text="Cancel" onClick={ this.onClickDiscard } />
+          <PrimaryButton text="Save" onClick={ this.onClickSave } className="save-button" disabled={ clean } />
         </Row>
       </GenericDocument>
     );
