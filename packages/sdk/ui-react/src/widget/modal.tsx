1fa0d3b38134219803397bb5b46cb41a2c88df0c
Tony Anziano <toanzian@microsoft.com>
2018-05-14 12:58:56 -0700

    Added tab index to widget/modal component's close icon so it works with the focus trap.

--- packages/sdk/ui-react/src/widget/modal.tsx
+++ packages/sdk/ui-react/src/widget/modal.tsx
@@ -71,7 +71,7 @@ export class Modal extends Component<ModalProps, {}> {
     const { closeButtonCssOverrides = {}, title = '', cancel } = this.props;
     return (
       <header>
-        <button { ...merge(this.closeButtonCss, closeButtonCssOverrides) } onClick={ cancel }>
+        <button tabIndex={ 0 } { ...merge(this.closeButtonCss, closeButtonCssOverrides) } onClick={ cancel }>
           <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='1 1 16 16'>
             <g>
               <polygon points="14.1015625 2.6015625 8.7109375 8 14.1015625 13.3984375 13.3984375 14.1015625 8 8.7109375 2.6015625 14.1015625 1.8984375 13.3984375 7.2890625 8 1.8984375 2.6015625 2.6015625 1.8984375 8 7.2890625 13.3984375 1.8984375"/>
@@ -115,19 +115,23 @@ export class Modal extends Component<ModalProps, {}> {
           padding: '5px 0 0 0',
           maxHeight: '96px',
           overflow: 'auto',
+
           '> li': {
             padding: '1px 11px',
             backgroundColor: '#efefef',
             display: 'flex',
+
             '& span': {
               color: '#777',
               width: '100%',
+
               '&:last-child': {
                 textAlign: 'right',
                 width: '75%',
                 paddingRight: '9px'
               }
             },
+
             '&:nth-child(odd)': {
               backgroundColor: 'white'
             },
@@ -139,10 +143,12 @@ export class Modal extends Component<ModalProps, {}> {
         position: 'absolute',
         right: '24px',
         bottom: '32px',
+
         '> button + button': {
           marginLeft: '8px'
         }
       },
+
       '& .selectAll': {
         padding: '5px 11px',
       },
@@ -150,6 +156,7 @@ export class Modal extends Component<ModalProps, {}> {
       '& .checkboxOverride': {
         display: 'inline-block',
         width: '150px',
+
         '> label': {
           overflow: 'hidden',
           textOverflow: 'ellipsis',
@@ -174,8 +181,14 @@ export class Modal extends Component<ModalProps, {}> {
       top: '12px',
       width: '16px',
       height: '16px',
+
+      '&:focus': {
+        outline: `1px dashed ${Colors.C2}`
+      },
+
       '> svg': {
         fill: Colors.C3,
+
         '&:hover': {
           fill: Colors.C12
         }
