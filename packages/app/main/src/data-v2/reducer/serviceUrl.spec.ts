6bee85d3efe9602ff7e7c49e6458740ed585ceeb
Justin Wilaby <juwilaby@microsoft.com>
2018-06-04 14:20:45 -0700

    Restored file deleted from bad merge

--- packages/app/main/src/data-v2/reducer/serviceUrl.spec.ts
+++ packages/app/main/src/data-v2/reducer/serviceUrl.spec.ts
@@ -5,7 +5,7 @@ import { getStore } from '../store';
 describe('The serviceUrl reducer', () => {
   let store: Store<State> = getStore();
 
-  test('should updated the store when a new serviceUrl action is dispatched', () => {
+  test('should update the store when a new serviceUrl action is dispatched', () => {
     store.dispatch({ type: 'updateServiceUrl', payload: 'http://localhost' });
     expect(store.getState().serviceUrl).toEqual('http://localhost');
   });
