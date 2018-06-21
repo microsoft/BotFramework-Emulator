e39facd50adb1809316fd49bef1bf4016df1d0eb
Justin Wilaby <juwilaby@microsoft.com>
2018-06-21 16:52:55 -0700

    light, dark and hight contrast themes for the navBar

--- packages/app/client/src/ui/shell/navBar/navBar.scss.d.ts
+++ packages/app/client/src/ui/shell/navBar/navBar.scss.d.ts
@@ -1,8 +1,5 @@
 // This is a generated file. Changes are likely to result in being overwritten
 export const navBar: string;
-export const services: string;
-export const botSettings: string;
-export const settings: string;
 export const navLink: string;
 export const selected: string;
 export const disabled: string;
