4be5db7298c8bfc91ae67218c0f2b92068c05bf0
Enzo Cano <enzo.cano@southworks.com>
2018-08-24 17:06:39 -0300

    Generate releases

--- generate_releases.sh
+++ generate_releases.sh
@@ -1,1 +1,25 @@
 echo Pack releases
+
+mkdir releases
+
+cd ./packages/app/main/
+
+npm version $TRAVIS_TAG --allow-same-version
+
+gulp package:windows-nsis
+gulp package:windows-squirrel
+
+gulp package:mac
+
+gulp package:linux
+
+cd ./dist/
+
+echo Files to dist
+
+for filename in *; do
+  if [[ $filename = *"$TRAVIS_TAG"* ]]; then
+    echo $filename
+    cp "$filename" ./../../../../releases/
+  fi
+done
