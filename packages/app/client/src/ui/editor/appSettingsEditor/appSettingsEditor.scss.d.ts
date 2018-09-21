60a268b322e303cfa8f1b4746f09d8b9a1c64403
Tony Anziano <toanzian@microsoft.com>
2018-09-20 17:11:25 -0700

    Improved spacing / sizing in app settings page.

--- packages/app/client/src/ui/editor/appSettingsEditor/appSettingsEditor.scss.d.ts
+++ packages/app/client/src/ui/editor/appSettingsEditor/appSettingsEditor.scss.d.ts
@@ -1,9 +1,11 @@
 // This is a generated file. Changes are likely to result in being overwritten
 export const appSettingsEditor: string;
+export const marginBottomRow: string;
 export const rightColumn: string;
 export const buttonRow: string;
 export const browseButton: string;
 export const saveButton: string;
+export const cancelButton: string;
 export const sizeLimitSuffix: string;
 export const checkboxOverrides: string;
 export const appSettingsInput: string;
