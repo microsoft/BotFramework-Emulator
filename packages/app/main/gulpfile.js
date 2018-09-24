9e97af217578ad4cf7efeb132f561d163477ace0
Tony Anziano <toanzian@microsoft.com>
2018-09-24 13:11:09 -0700

    Modified linux artifacts names.

--- packages/app/main/gulpfile.js
+++ packages/app/main/gulpfile.js
@@ -694,7 +694,7 @@ function setReleaseFilename(filename, options = {}) {
   }
   if (options.fixBasename) {
     // filename = filename.replace(/bot[-|\s]framework/ig, 'botframework');
-    filename = filename.replace(/(bot[-|\s]framework)?(main)?/, 'bot-framework-emulator');
+    filename = filename.replace(/(bot[-|\s]framework)?(main)?/, 'botframework-emulator');
   }
   return filename;
 }
