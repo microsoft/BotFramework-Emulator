318380a3e467b7f45fcd4c90f90c991423176acd
Tony Anziano <toanzian@microsoft.com>
2018-10-08 16:09:51 -0700

    Added missing constants.

--- packages/app/main/gulpfile.common.js
+++ packages/app/main/gulpfile.common.js
@@ -1,5 +1,11 @@
 const packageJson = require('./package.json');
 
+const defaultElectronMirror = 'https://github.com/electron/electron/releases/download/v';
+const defaultElectronVersion = pjson.devDependencies["electron"];
+const githubAccountName = "Microsoft";
+const githubRepoName = "BotFramework-Emulator";
+const appId = "F3C061A6-FE81-4548-82ED-C1171D9856BB";
+
 /** Gets an environment variable with the provided name */
 function getEnvironmentVar(name, defaultValue = undefined) {
   return (process.env[name] === undefined) ? defaultValue : process.env[name]
