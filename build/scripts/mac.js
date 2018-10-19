945c201f62ea17e3dd5920a405a6caa7131a99c6
Tony Anziano <toanzian@microsoft.com>
2018-10-19 14:43:59 -0700

    WIP Mac V3 build.

--- build/scripts/mac.js
+++ build/scripts/mac.js
@@ -1,24 +1,22 @@
 const { getEnvVar } = require('./utils');
 const packageJson = require('../../package.json');
-const platform = getEnvVar('EMU_PLATFORM', 'windows');
+const platform = getEnvVar('EMU_PLATFORM', 'mac');
 const version = getEnvVar('EMU_VERSION', packageJson.version);
-const releaseFileName = `${packageJson.name}-${version}-${platform}-setup.exe`;
+const releaseFileName = `${packageJson.name}-${version}-${platform}.zip`;
 
-writeLatestYml().catch(e => console.error(`Error while trying to write latest.yml: ${e}`));
+writeLatestYml().catch(e => console.error(`Error while trying to write latest-mac.yml: ${e}`));
 
 /** Creates a checksum for the packaged installer and generates latest.yml */
 async function writeLatestYml() {
   const sha512 = await hashFileAsync(`./dist/${releaseFileName}`);
-  const sha2 = await hashFileAsync(`./dist/${releaseFileName}`, 'sha256', 'hex');
   const releaseDate = new Date().toISOString();
 
   writeYamlMetadataFile(
     releaseFileName,
-    'latest.yml',
+    'latest-mac.yml',
     './dist',
     sha512,
-    releaseDate,
-    { sha2 }
+    releaseDate
   );
 }
 
@@ -29,7 +27,7 @@ function hashFileAsync(filename, algo = 'sha512', encoding = 'base64') {
 }
 
 /** Generates the .yml metadata file */
-function writeYamlMetadataFile(releaseFilename, yamlFilename, path, fileHash, releaseDate, extra = {}) {
+function writeYamlMetadataFile(releaseFilename, yamlFilename, path, fileHash, releaseDate) {
   const fsp = require('fs-extra');
   const yaml = require('js-yaml');
 
@@ -40,10 +38,6 @@ function writeYamlMetadataFile(releaseFilename, yamlFilename, path, fileHash, re
     path: releaseFilename,
     sha512: fileHash
   };
-  const obj = {
-    ...ymlInfo,
-    ...extra
-  };
-  const ymlStr = yaml.safeDump(obj);
+  const ymlStr = yaml.safeDump(ymlInfo);
   fsp.writeFileSync(`./${path}/${yamlFilename}`, ymlStr);
 }
