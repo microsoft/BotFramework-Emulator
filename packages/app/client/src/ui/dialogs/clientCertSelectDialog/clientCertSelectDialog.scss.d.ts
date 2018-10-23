1caa4f6fa255bafb9747a6e06bcfc3f0b981f4e5
Corina <v-corgum@microsoft.com>
2018-10-23 14:02:27 -0700

    styling for client cert select dialog

--- packages/app/client/src/ui/dialogs/clientCertSelectDialog/clientCertSelectDialog.scss.d.ts
+++ packages/app/client/src/ui/dialogs/clientCertSelectDialog/clientCertSelectDialog.scss.d.ts
@@ -1,2 +1,5 @@
 // This is a generated file. Changes are likely to result in being overwritten
 export const certList: string;
+export const certIcon: string;
+export const certInfo: string;
+export const selectedCert: string;
