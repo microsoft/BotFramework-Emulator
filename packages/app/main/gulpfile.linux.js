06c0e0bdc98162d2f28eca8e1021cce9481274de
Tony Anziano <toanzian@microsoft.com>
2018-10-25 13:21:32 -0700

    Added linux build step to generate latest-linux.yml and pruned unused files.

--- packages/app/main/gulpfile.linux.js
+++ packages/app/main/gulpfile.linux.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const common = require('./gulpfile.common.js');
+const packageJson = require('./package.json');
 
 /** Package the emulator using electron-builder
  *  and output artifacts to /dist/
@@ -29,3 +30,37 @@ gulp.task('package', async () => {
       .on('end', resolve);
   });
 });
+
+/** Generates latest-linux.yml */
+gulp.task('redist:metadata-only', async () => {
+  const { hashFileAsync, getReleaseFilename } = common;
+  const releaseFileName = `${getReleaseFilename()}.AppImage`;
+  const sha512 = await hashFileAsync(`./dist/${releaseFileName}`);
+  const releaseDate = new Date().toISOString();
+
+  writeYamlMetadataFile(
+    releaseFileName,
+    'latest-linux.yml',
+    './dist',
+    sha512,
+    releaseDate
+  );
+});
+
+/** Writes the .yml metadata file */
+function writeYamlMetadataFile(releaseFilename, yamlFilename, path, fileHash, releaseDate) {
+  const { getEnvironmentVar } = common;
+  var fsp = require('fs-extra');
+  var yaml = require('js-yaml');
+  const version = getEnvironmentVar('EMU_VERSION', packageJson.version);
+
+  const ymlInfo = {
+    version,
+    releaseDate,
+    githubArtifactName: releaseFilename,
+    path: releaseFilename,
+    sha512: fileHash
+  };
+  const ymlStr = yaml.safeDump(ymlInfo);
+  fsp.writeFileSync(`./${path}/${yamlFilename}`, ymlStr);
+}
