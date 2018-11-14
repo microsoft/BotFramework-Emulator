41a8f4913ac7cf52ace26e1efbc145b35be39614
justinwilaby <jwilaby@gmail.com>
2018-11-14 11:05:54 -0800

    formatting and cleanup

--- packages/app/client/src/vendors.ts
+++ packages/app/client/src/vendors.ts
@@ -5,8 +5,6 @@ import 'react-dom';
 import 'redux-promise-middleware';
 
 import 'base64url';
-import 'botframework-webchat/built/SpeechModule';
-import 'botframework-webchat/built/Chat';
 
 import 'botframework-config/lib/schema';
 import 'botframework-config/lib/models';
