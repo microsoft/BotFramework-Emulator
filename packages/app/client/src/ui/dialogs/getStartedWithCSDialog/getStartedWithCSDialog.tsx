c108f8bdd224e5263edeccc4232d611bbe4d3429
Justin Wilaby <jwilaby@gmail.com>
2018-12-18 13:57:19 -0800

    included blob storage

--- packages/app/client/src/ui/dialogs/getStartedWithCSDialog/getStartedWithCSDialog.tsx
+++ packages/app/client/src/ui/dialogs/getStartedWithCSDialog/getStartedWithCSDialog.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import { Component } from 'react';
-import { ServiceTypes } from 'botframework-config/lib/schema';
 import { DefaultButton, Dialog, DialogFooter, PrimaryButton } from '@bfemulator/ui-react';
+import { ServiceTypes } from 'botframework-config/lib/schema';
+import * as React from 'react';
+import { Component, ReactNode } from 'react';
 import { serviceTypeLabels } from '../../../utils/serviceTypeLables';
 import * as styles from '../dialogStyles.scss';
 
@@ -15,80 +15,90 @@ export interface GetStartedWithCSDialogProps {
 }
 
 const titleMap = {
-  [ ServiceTypes.Luis ]: 'Create a LUIS app',
-  [ ServiceTypes.Dispatch ]: 'Connect to a Dispatch model',
-  [ ServiceTypes.QnA ]: 'Create a QnA Maker knowledge base'
+  [ServiceTypes.Luis]: 'Create a LUIS app',
+  [ServiceTypes.Dispatch]: 'Connect to a Dispatch model',
+  [ServiceTypes.QnA]: 'Create a QnA Maker knowledge base',
+  [ServiceTypes.BlobStorage]: 'Create a Blob Storage Container'
 };
 
 const buttonTextMap = {
-  [ ServiceTypes.Luis ]: 'LUIS',
-  [ ServiceTypes.Dispatch ]: 'LUIS',
-  [ ServiceTypes.QnA ]: 'QnA Maker'
+  [ServiceTypes.Luis]: 'LUIS',
+  [ServiceTypes.Dispatch]: 'LUIS',
+  [ServiceTypes.QnA]: 'QnA Maker',
+  [ServiceTypes.BlobStorage]: 'Blob Storage'
 };
 
 export class GetStartedWithCSDialog extends Component<GetStartedWithCSDialogProps, {}> {
   public render() {
     return (
       <Dialog
-        className={styles.dialogMedium}
+        className={ styles.dialogMedium }
         cancel={ this.props.cancel }
-        title={ titleMap[ this.props.serviceType ] }>
+        title={ titleMap[this.props.serviceType] }>
         { this.content }
         <DialogFooter>
           <DefaultButton text="Cancel" onClick={ this.props.cancel }/>
-          <PrimaryButton text={ `Go to ${ buttonTextMap[ this.props.serviceType ] }` }
+          <PrimaryButton text={ `Go to ${ buttonTextMap[this.props.serviceType] }` }
                          onClick={ this.props.confirm }/>
         </DialogFooter>
       </Dialog>
     );
   }
 
-  private get content(): JSX.Element {
+  private get content(): ReactNode {
     const { serviceType } = this.props;
 
     switch (serviceType) {
       case ServiceTypes.Luis:
         return this.luisContent;
+
       case ServiceTypes.Dispatch:
         return this.dispatchContent;
+
       case ServiceTypes.QnA:
         return this.qnaContent;
+
+      case ServiceTypes.BlobStorage:
+        return this.blobContent;
 
       default:
         return null;
     }
   }
 
-  private get luisContent(): JSX.Element {
-    const { showNoModelsFoundContent, authenticatedUser, serviceType } = this.props;
-    const label = serviceTypeLabels[ serviceType ];
-    if (!showNoModelsFoundContent) {
-      return (
-        <>
-          <p>
-            { 'Language Understanding Service (LUIS) is a matching learning-based service for adding language ' +
-            'understanding to bots, applications and IoT devices.' }
-          </p>
-          <p>
-            { `You have not signed up for a LUIS account under ${this.props.authenticatedUser} ` }
-            <a href="http://aka.ms/bot-framework-emulator-LUIS-docs-home">
-              Learn more about LUIS
-            </a>
-          </p>
-          <p>
-            { 'Alternatively, you can ' }
-            <a href="javascript:void(0);" onClick={ this.props.launchConnectedServiceEditor }>
-              connect to a LUIS app manually
-            </a>
-            { ' if you know the app ID, version, and authoring key.' }
-          </p>
-        </>
-      );
+  private get luisContent(): ReactNode {
+    if (this.props.showNoModelsFoundContent) {
+      return this.luisNoModelsFoundContent;
     }
     return (
       <>
         <p>
-          Signed in as { authenticatedUser }.
+          { 'Language Understanding Service (LUIS) is a matching learning-based service for adding language ' +
+          'understanding to bots, applications and IoT devices.' }
+        </p>
+        <p>
+          { `You have not signed up for a LUIS account under ${ this.props.authenticatedUser } ` }
+          <a href="http://aka.ms/bot-framework-emulator-LUIS-docs-home">
+            Learn more about LUIS
+          </a>
+        </p>
+        <p>
+          { 'Alternatively, you can ' }
+          <a href="javascript:void(0);" onClick={ this.props.launchConnectedServiceEditor }>
+            connect to a LUIS app manually
+          </a>
+          { ' if you know the app ID, version, and authoring key.' }
+        </p>
+      </>
+    );
+  }
+
+  private get luisNoModelsFoundContent(): ReactNode {
+    const label = serviceTypeLabels[this.props.serviceType];
+    return (
+      <>
+        <p>
+          Signed in as { this.props.authenticatedUser }.
         </p>
         <p>
           { 'You do not have any {label} models associated with this account. ' }
@@ -108,40 +118,41 @@ export class GetStartedWithCSDialog extends Component<GetStartedWithCSDialogProp
     );
   }
 
-  private get dispatchContent(): JSX.Element {
-    const { showNoModelsFoundContent, authenticatedUser } = this.props;
-
-    if (!showNoModelsFoundContent) {
-
-      return (
-        <>
-          <p>
-            { 'A Dispatch model is a LUIS model that enables your bot to dispatch intents across multiple LUIS ' +
-            'apps and QnAMaker knowledge bases. ' }
-            <a href="https://aka.ms/bot-framework-emulator-create-dispatch">
-              Learn more about Dispatch models
-            </a>
-          </p>
-          <p>
-            { `You have not signed up for a LUIS account under ${this.props.authenticatedUser} ` }
-            <a href="http://aka.ms/bot-framework-emulator-LUIS-docs-home">
-              Learn more about LUIS
-            </a>
-          </p>
-          <br/>
-          { 'Alternatively, you can ' }
-          <a href="javascript:void(0);" onClick={ this.props.launchConnectedServiceEditor }>
-            connect to a Dispatch app manually
-          </a>
-          { ' if you know the app ID, version, and authoring key.' }
-        </>
-      );
+  private get dispatchContent(): ReactNode {
+    if (this.props.showNoModelsFoundContent) {
+      return this.dispatchNoModelsFoundContent;
     }
 
     return (
       <>
         <p>
-          Signed in as { authenticatedUser }.
+          { 'A Dispatch model is a LUIS model that enables your bot to dispatch intents across multiple LUIS ' +
+          'apps and QnAMaker knowledge bases. ' }
+          <a href="https://aka.ms/bot-framework-emulator-create-dispatch">
+            Learn more about Dispatch models
+          </a>
+        </p>
+        <p>
+          { `You have not signed up for a LUIS account under ${ this.props.authenticatedUser } ` }
+          <a href="http://aka.ms/bot-framework-emulator-LUIS-docs-home">
+            Learn more about LUIS
+          </a>
+        </p>
+        <br/>
+        { 'Alternatively, you can ' }
+        <a href="javascript:void(0);" onClick={ this.props.launchConnectedServiceEditor }>
+          connect to a Dispatch app manually
+        </a>
+        { ' if you know the app ID, version, and authoring key.' }
+      </>
+    );
+  }
+
+  private get dispatchNoModelsFoundContent(): ReactNode {
+    return (
+      <>
+        <p>
+          Signed in as { this.props.authenticatedUser }.
         </p>
         <p>
           { 'You do not have any Dispatch models associated with this account. ' }
@@ -164,22 +175,45 @@ export class GetStartedWithCSDialog extends Component<GetStartedWithCSDialogProp
     );
   }
 
-  private get qnaContent(): JSX.Element {
+  private get qnaContent(): ReactNode {
     return (
       <>
         <p>
           { 'QnA Maker is a service that creates a question-and-answer knowledge base from FAQs and product manuals.' }
         </p>
         <p>
-          { `You have not signed up for a QnA Maker account under ${this.props.authenticatedUser}. ` }
+          { `You have not signed up for a QnA Maker account under ${ this.props.authenticatedUser }. ` }
           <a href="javascript:void(0)">Get started with QnA Maker</a>
         </p>
         <p>
           { ' Alternatively, you can ' }
-          <a href="javascript:void(0);" onClick={ this.props.launchConnectedServiceEditor }>
+          <a href="https://aka.ms/bot-framework-emulator-qna-docs-home"
+             onClick={ this.props.launchConnectedServiceEditor }>
             connect to a knowledge base manually
           </a>
           { ' if you know the ID and subscription key.' }
+        </p>
+      </>
+    );
+  }
+
+  private get blobContent(): ReactNode {
+    return (
+      <>
+        <p>
+          { 'Blob Storage is a service that allows your to store unstructured ' +
+          'data and is used to store a Bot\'s transcripts.' }
+        </p>
+        <p>
+          { `You have do not have a Blob container under ${ this.props.authenticatedUser }. ` }
+          <a href="https://azure.microsoft.com/en-us/services/storage/blobs/">Get started with Blob Storage</a>
+        </p>
+        <p>
+          { ' Alternatively, you can ' }
+          <a href="javascript:void(0);" onClick={ this.props.launchConnectedServiceEditor }>
+            connect to a Blob container manually
+          </a>
+          { ' if you know the ID, subscription key, container name and connection string.' }
         </p>
       </>
     );
