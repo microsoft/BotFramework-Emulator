80fca33538ad349dcf95dda0e47e0df50817db0c
Justin Wilaby <jwilaby@gmail.com>
2018-12-18 13:57:19 -0800

    included blob storage

--- packages/app/client/src/ui/dialogs/getStartedWithCSDialog/getStartedWithCSDialog.spec.tsx
+++ packages/app/client/src/ui/dialogs/getStartedWithCSDialog/getStartedWithCSDialog.spec.tsx
@@ -1,12 +1,12 @@
+import { ServiceTypes } from 'botframework-config/lib/schema';
+import { mount } from 'enzyme';
 import * as React from 'react';
 import { Provider } from 'react-redux';
-import { mount } from 'enzyme';
-import { GetStartedWithCSDialogContainer } from './getStartedWithCSDialogContainer';
 import { combineReducers, createStore } from 'redux';
+import { azureArmTokenDataChanged } from '../../../data/action/azureAuthActions';
 import { azureAuth } from '../../../data/reducer/azureAuthReducer';
 import { GetStartedWithCSDialog } from './getStartedWithCSDialog';
-import { azureArmTokenDataChanged } from '../../../data/action/azureAuthActions';
-import { ServiceTypes } from 'botframework-config/lib/schema';
+import { GetStartedWithCSDialogContainer } from './getStartedWithCSDialogContainer';
 
 jest.mock('../service', () => ({
   DialogService: {
@@ -27,55 +27,84 @@ jest.mock('../../dialogs/', () => ({
 
 describe('The GetStartedWithCSDialog component should', () => {
   let mockStore;
-  let parent;
   const mockArmToken = 'bm90aGluZw==.eyJ1cG4iOiJnbGFzZ293QHNjb3RsYW5kLmNvbSJ9.7gjdshgfdsk98458205jfds9843fjds';
   beforeEach(() => {
     mockStore = createStore(combineReducers({ azureAuth }));
     mockStore.dispatch(azureArmTokenDataChanged(mockArmToken));
-    parent = mount(
-      <Provider store={ mockStore }>
-        <GetStartedWithCSDialogContainer serviceType={ ServiceTypes.Luis } />
-      </Provider>
-    );
   });
 
   it('should render deeply', () => {
+    const parent = mount(
+      <Provider store={ mockStore }>
+        <GetStartedWithCSDialogContainer serviceType={ ServiceTypes.Luis }/>
+      </Provider>
+    );
     expect(parent.find(GetStartedWithCSDialogContainer)).not.toBe(null);
   });
 
   it('should contain both a cancel and confirm function in the props', () => {
+    const parent = mount(
+      <Provider store={ mockStore }>
+        <GetStartedWithCSDialogContainer serviceType={ ServiceTypes.Luis }/>
+      </Provider>
+    );
     const prompt = parent.find(GetStartedWithCSDialog);
     expect(typeof (prompt.props() as any).cancel).toBe('function');
     expect(typeof (prompt.props() as any).confirm).toBe('function');
     expect(typeof (prompt.props() as any).launchConnectedServiceEditor).toBe('function');
   });
 
+  it('should display luisNoModelsFoundContent when the ServiceTypes.Luis and ' +
+    'showNoModelsFoundContent is provided in the props', () => {
+    const parent: any = mount(
+      <Provider store={ mockStore }>
+        <GetStartedWithCSDialogContainer showNoModelsFoundContent={ true } serviceType={ ServiceTypes.Luis }/>
+      </Provider>
+    );
+    const prompt = parent.find(GetStartedWithCSDialog);
+    expect(prompt.content).toBe(prompt.luisNoModelsFoundContent);
+  });
+
   it('should display luisContent when the ServiceTypes.Luis is provided in the props', () => {
+    const parent: any = mount(
+      <Provider store={ mockStore }>
+        <GetStartedWithCSDialogContainer serviceType={ ServiceTypes.Luis }/>
+      </Provider>
+    );
     const prompt = parent.find(GetStartedWithCSDialog);
     expect(prompt.content).toBe(prompt.luisContent);
   });
 
-  it('should display luisContent when the ServiceTypes.Dispatch is provided in the props', () => {
-    parent = mount(<Provider store={ mockStore }>
-      <GetStartedWithCSDialogContainer serviceType={ ServiceTypes.Dispatch } />
+  it('should display dispatchContent when the ServiceTypes.Dispatch is provided in the props', () => {
+    const parent: any = mount(<Provider store={ mockStore }>
+      <GetStartedWithCSDialogContainer serviceType={ ServiceTypes.Dispatch }/>
     </Provider>);
     const prompt = parent.find(GetStartedWithCSDialog);
-    expect(prompt.content).toBe(prompt.luisContent);
+    expect(prompt.content).toBe(prompt.dispatchContent);
+  });
+
+  it('should display dispatchNoModelsFoundContent when the ServiceTypes.Dispatch and ' +
+    'showNoModelsFoundContent is provided in the props', () => {
+    const parent: any = mount(<Provider store={ mockStore }>
+      <GetStartedWithCSDialogContainer showNoModelsFoundContent={ true } serviceType={ ServiceTypes.Dispatch }/>
+    </Provider>);
+    const prompt = parent.find(GetStartedWithCSDialog);
+    expect(prompt.content).toBe(prompt.dispatchNoModelsFoundContent);
   });
 
   it('should display qnaContent when the ServiceTypes.QnA is provided in the props', () => {
-    parent = mount(<Provider store={ mockStore }>
-      <GetStartedWithCSDialogContainer serviceType={ ServiceTypes.QnA } />
+    const parent: any = mount(<Provider store={ mockStore }>
+      <GetStartedWithCSDialogContainer serviceType={ ServiceTypes.QnA }/>
     </Provider>);
     const prompt = parent.find(GetStartedWithCSDialog);
     expect(prompt.content).toBe(prompt.qnaContent);
   });
 
-  it('should display the "showNoModelsFoundContent" when specified in the props', () => {
-    parent = mount(<Provider store={ mockStore }>
-      <GetStartedWithCSDialogContainer serviceType={ ServiceTypes.QnA } showNoModelsFoundContent={ true } />
+  it('should display blobContent when the ServiceTypes.BlobStorage is provided in the props', () => {
+    const parent: any = mount(<Provider store={ mockStore }>
+      <GetStartedWithCSDialogContainer serviceType={ ServiceTypes.BlobStorage }/>
     </Provider>);
     const prompt = parent.find(GetStartedWithCSDialog);
-    expect(prompt.content).toBe(prompt.qnaContent);
+    expect(prompt.content).toBe(prompt.blobContent);
   });
 });
