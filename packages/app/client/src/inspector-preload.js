7b7a38887a14d4bf40ea04523b6b4193322245a8
Andy <asbrown002@gmail.com>
2019-01-17 10:45:12 -0800

    revert converting to import

--- packages/app/client/src/inspector-preload.js
+++ packages/app/client/src/inspector-preload.js
@@ -31,7 +31,8 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-import { ipcRenderer, remote } from 'electron';
+// eslint-disable-next-line typescript/no-var-requires
+const { ipcRenderer, remote } = require('electron');
 
 ipcRenderer.on('inspect', (sender, obj) => {
   window.host.dispatch('inspect', obj);
