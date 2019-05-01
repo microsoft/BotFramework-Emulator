c406632cc880b40c0e8656509994481ff925a8bd
Andy Brown <asbrown002@gmail.com>
2019-05-01 12:20:57 -0700

    encode src

--- packages/app/client/src/ui/editor/emulator/parts/inspector/inspector.tsx
+++ packages/app/client/src/ui/editor/emulator/parts/inspector/inspector.tsx
@@ -271,13 +271,14 @@ export class Inspector extends React.Component<InspectorProps, InspectorState> {
     if (!src) {
       return;
     }
+    const encodedSrc = encodeURI(src);
     const { webViewByLocation: webViews } = this;
-    if (!webViews[src]) {
-      webViews[src] = this.createWebView(state);
+    if (!webViews[encodedSrc]) {
+      webViews[encodedSrc] = this.createWebView(state);
     } else {
       this.sendInitializationStackToInspector();
     }
-    const nextInspector = webViews[src] || (webViews[src] = this.createWebView(state));
+    const nextInspector = webViews[encodedSrc] || (webViews[encodedSrc] = this.createWebView(state));
     nextInspector.style.display = '';
 
     const { containerRef } = this.state;
@@ -311,7 +312,7 @@ export class Inspector extends React.Component<InspectorProps, InspectorState> {
     webView.addEventListener('ipc-message', this.ipcMessageEventHandler);
     webView.setAttribute('partition', `persist:${state.botHash}`);
     webView.setAttribute('preload', preload);
-    webView.setAttribute('src', state.inspector.src);
+    webView.setAttribute('src', encodeURI(state.inspector.src));
     return webView;
   }
 
@@ -435,8 +436,8 @@ export class Inspector extends React.Component<InspectorProps, InspectorState> {
 
   private sendToInspector(channel: string, ...args: any[]) {
     const { inspectorSrc } = this.state;
-    const inspector = this.webViewByLocation[inspectorSrc];
-    if (!inspector || !this.domReadyByLocation[inspectorSrc]) {
+    const inspector = this.webViewByLocation[encodeURI(inspectorSrc)];
+    if (!inspector || !this.domReadyByLocation[encodeURI(inspectorSrc)]) {
       return;
     }
     try {
