2147edbe86da5f69bbaec062f9f60025f4a4adfb
Justin Wilaby <jwilaby@gmail.com>
2019-05-08 11:04:48 -0700

    Added a mouseshield to prevent the iframes from eating mouse events

--- packages/app/client/src/ui/editor/emulator/parts/inspector/inspector.tsx
+++ packages/app/client/src/ui/editor/emulator/parts/inspector/inspector.tsx
@@ -32,7 +32,8 @@
 //
 
 // Cheating here and pulling in a module from node. Can be easily replaced if we ever move the emulator to the web.
-import { join, normalize } from 'path';
+// @ts-ignore
+import inspectPreload from '!raw-loader!./inspector-preload.js';
 
 import {
   ExtensionInspector,
@@ -299,17 +300,14 @@ export class Inspector extends React.Component<InspectorProps, InspectorState> {
   }
 
   private createWebView(state: InspectorState): ElectronHTMLWebViewElement {
-    const appPath = normalize(this.props.appPath);
-    const preload = normalize(join('file://', appPath, '/node_modules/@bfemulator/client/public/inspector-preload.js'));
-    const webView: ElectronHTMLWebViewElement = document.createElement('webview');
-
+    const webView: any = document.createElement('webview');
+    webView.executeJavaScript(inspectPreload);
     webView.className = styles.webViewContainer;
     webView.addEventListener('dragenter', this.onInspectorDrag, true);
     webView.addEventListener('dragover', this.onInspectorDrag, true);
     webView.addEventListener('ipc-message', this.ipcMessageEventHandler);
     webView.setAttribute('partition', `persist:${state.botHash}`);
-    webView.setAttribute('preload', preload);
-    webView.setAttribute('src', encodeURI(state.inspector.src));
+
     return webView;
   }
 
