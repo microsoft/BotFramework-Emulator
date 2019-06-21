d5a4091839522b103da49f8d0544f67e1b2af924
Billy Delgado <37461749+Bill7zz@users.noreply.github.com>
2019-06-21 18:31:18 -0300

    Add openUrlDialog component

---
+++ packages/app/client/src/ui/dialogs/openUrlDialog/openUrlDialog.tsx
@@ -0,0 +1,63 @@
+///
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+//
+// Microsoft Bot Framework: http://botframework.com
+//
+// Bot Framework Emulator Github:
+// https://github.com/Microsoft/BotFramwork-Emulator
+//
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+//
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+import { Dialog, DialogFooter, DefaultButton, PrimaryButton } from '@bfemulator/ui-react';
+import * as React from 'react';
+import { Component } from 'react';
+
+import * as styles from './openUrlDialog.scss';
+
+export interface OpenUrlDialogProps {
+  url?: string;
+  cancel: () => void;
+  confirm: () => void;
+}
+
+export class OpenUrlDialog extends Component<OpenUrlDialogProps, {}> {
+  constructor(props: OpenUrlDialogProps) {
+    super(props);
+  }
+
+  public render() {
+    return (
+      <Dialog cancel={this.props.cancel} className={styles.openUrlDialog} title="Confirm Open URL">
+        <p>{'\n\n Do you want to open this URL?'}</p>
+        <p>{'\n' + this.props.url}</p>
+        <DialogFooter>
+          <DefaultButton text="Cancel" type="button" onClick={this.props.cancel} />
+          <PrimaryButton text="Confirm" type="button" onClick={this.props.confirm} />
+        </DialogFooter>
+      </Dialog>
+    );
+  }
+}
