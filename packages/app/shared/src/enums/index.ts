be42d06edd68a84dc7f559475d31eeaf90e971c4
Tony Anziano <tonyanziano5@gmail.com>
2019-07-08 13:41:50 -0700

    Fixed an issue caused by filename casing on Linux.

--- packages/app/shared/src/enums/index.ts
+++ packages/app/shared/src/enums/index.ts
@@ -30,5 +30,5 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-export * from './ServiceCodes';
-export * from './ValueTypes';
+export * from './serviceCodes';
+export * from './valueTypes';
