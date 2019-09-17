c1a86be4a7f80df657bdfd87242aaa6bc2b392d4
Corina <14900841+corinagum@users.noreply.github.com>
2019-09-17 14:22:27 -0700

    Create <LinkButton> component

--- packages/sdk/ui-react/src/widget/button/button.scss.d.ts
+++ packages/sdk/ui-react/src/widget/button/button.scss.d.ts
@@ -1,4 +1,5 @@
 // This is a generated file. Changes are likely to result in being overwritten
 export const button: string;
 export const defaultButton: string;
+export const linkButton: string;
 export const primaryButton: string;
---
+++ packages/sdk/ui-react/src/widget/button/linkButton.tsx
@@ -0,0 +1,72 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+//
+// Microsoft Bot Framework: http://botframework.com
+//
+// Bot Framework Emulator Github:
+// https://github.com/Microsoft/BotFramwork-Emulator
+//
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+//
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+import * as React from 'react';
+
+import * as styles from './button.scss';
+
+export interface LinkButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  ariaLabel?: string;
+  linkRole?: boolean;
+  text?: string;
+}
+
+// This UI component is intended to function as a button but has the appearance of a link.
+// Since we want Emulator to open links the user's default browser, all anchors in this app
+// should be a <LinkButton>, which will maintain the desired UX. Otherwise, CTRL + Click
+// or middle-mouse button click will open a new instance of Emulator.
+// Instead of <a href="...">Link name</a>, please use:
+// <LinkButton linkRole={true} onClick={...}>Link name</LinkButton>
+
+export class LinkButton extends React.Component<LinkButtonProps, {}> {
+  public render(): React.ReactNode {
+    const {
+      className: propsClassName = '',
+      ariaLabel = undefined,
+      linkRole = false,
+      text = undefined,
+      ...buttonProps
+    } = this.props;
+    const className = `${propsClassName} ${styles.linkButton}`;
+
+    const ariaLabelText = ariaLabel || text || (typeof this.props.children === 'string' && this.props.children);
+
+    if (!ariaLabelText) throw new Error('<LinkButton must have aria-label');
+
+    return (
+      <button {...buttonProps} aria-label={ariaLabelText} className={className} role={linkRole ? 'link' : 'button'}>
+        {text}
+        {this.props.children}
+      </button>
+    );
+  }
+}
