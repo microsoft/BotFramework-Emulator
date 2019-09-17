6f4816635598157693d4e9321ab38ccdc0c84995
Corina <14900841+corinagum@users.noreply.github.com>
2019-09-17 15:08:10 -0700

    Fix link colors for dialogs

--- packages/app/client/src/ui/dialogs/dialogStyles.scss.d.ts
+++ packages/app/client/src/ui/dialogs/dialogStyles.scss.d.ts
@@ -1,3 +1,4 @@
 // This is a generated file. Changes are likely to result in being overwritten
 export const dialogMedium: string;
 export const dialogLarge: string;
+export const dialogLink: string;
