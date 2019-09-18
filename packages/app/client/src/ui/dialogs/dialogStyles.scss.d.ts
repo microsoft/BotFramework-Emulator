d0c29e1049246887deaee291d9bff7689ba57ac5
Corina <14900841+corinagum@users.noreply.github.com>
2019-09-18 15:00:55 -0700

    azureLoginPromptDialog links to <LinkButton>

--- packages/app/client/src/ui/dialogs/dialogStyles.scss.d.ts
+++ packages/app/client/src/ui/dialogs/dialogStyles.scss.d.ts
@@ -1,8 +1,8 @@
 // This is a generated file. Changes are likely to result in being overwritten
 export const dialogMedium: string;
 export const dialogLarge: string;
-export const dialogLink: string;
 export const main: string;
+export const dialogLink: string;
 export const inputContainer: string;
 export const encryptKeyCheckBox: string;
 export const key: string;
