11df6f6388411442b287b50a583b3e4e8578df07
Corina <14900841+corinagum@users.noreply.github.com>
2019-09-19 10:24:00 -0700

    Resources UX links to <LinkButton>

--- packages/app/client/src/ui/dialogs/resourcesSettings/resourcesSettings.scss.d.ts
+++ packages/app/client/src/ui/dialogs/resourcesSettings/resourcesSettings.scss.d.ts
@@ -1,4 +1,4 @@
 // This is a generated file. Changes are likely to result in being overwritten
 export const inputContainer: string;
-export const browseAnchor: string;
+export const browseButton: string;
 export const container: string;
