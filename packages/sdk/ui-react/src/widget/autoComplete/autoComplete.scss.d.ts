2f79d1541c7f1b93dc11b6fec6c5610bef0fbdde
Tony Anziano <toanzian@microsoft.com>
2019-12-02 12:06:54 -0800

    Revert autocomplete dom changes.

--- packages/sdk/ui-react/src/widget/autoComplete/autoComplete.scss.d.ts
+++ packages/sdk/ui-react/src/widget/autoComplete/autoComplete.scss.d.ts
@@ -1,5 +1,4 @@
 // This is a generated file. Changes are likely to result in being overwritten
-export const container: string;
 export const autoCompleteContainer: string;
 export const selected: string;
 export const errorMessage: string;
