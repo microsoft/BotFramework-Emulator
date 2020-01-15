53f70f36cf1207ec8cf1724557296add33feb694
Tony Anziano <toanzian@microsoft.com>
2020-01-15 12:12:53 -0800

    Added some tests for uncovered code.

---
+++ packages/app/client/src/commands/fileCommands.spec.ts
@@ -0,0 +1,110 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+//
+// Microsoft Bot Framework: http://botframework.com
+//
+// Bot Framework Emulator Github:
+// https://github.com/Microsoft/BotFramwork-Emulator
+//
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+//
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+import { CommandServiceImpl, CommandRegistry, CommandServiceInstance } from '@bfemulator/sdk-shared';
+import { SharedConstants } from '@bfemulator/app-shared';
+
+import { addFile, removeFile, clear } from '../state/actions/fileActions';
+import { addDocPendingChange } from '../state/actions/editorActions';
+
+import { FileCommands } from './fileCommands';
+
+const mockDispatch = jest.fn();
+jest.mock('../state/store', () => ({
+  store: {
+    dispatch: action => mockDispatch(action),
+  },
+}));
+
+describe('The file commands', () => {
+  let commandService: CommandServiceImpl;
+  let registry: CommandRegistry;
+  const { File } = SharedConstants.Commands;
+
+  beforeAll(() => {
+    new FileCommands();
+    const decorator = CommandServiceInstance();
+    const descriptor = decorator({ descriptor: {} }, 'none') as any;
+    commandService = descriptor.descriptor.get();
+    registry = commandService.registry;
+  });
+
+  beforeEach(mockDispatch.mockClear);
+
+  it('should add a file to the store', () => {
+    const payload: any = {
+      type: 'leaf',
+      name: 'somefile.txt',
+      path: 'dir/somefile.txt',
+    };
+    const command = registry.getCommand(File.Add);
+    command(payload);
+
+    expect(mockDispatch).toHaveBeenCalledWith(addFile(payload));
+  });
+
+  it('should remove a file from the store', () => {
+    const payload: any = {
+      type: 'leaf',
+      name: 'somefile.txt',
+      path: 'dir/somefile.txt',
+    };
+    const command = registry.getCommand(File.Remove);
+    command(payload);
+
+    expect(mockDispatch).toHaveBeenCalledWith(removeFile(payload));
+  });
+
+  it('should clear the file store', () => {
+    const command = registry.getCommand(File.Clear);
+    command();
+
+    expect(mockDispatch).toHaveBeenCalledWith(clear());
+  });
+
+  it('should mark a file as changed (.chat file)', () => {
+    const filename = 'my-file.chat';
+    const command = registry.getCommand(File.Changed);
+    command(filename);
+
+    expect(mockDispatch).toHaveBeenCalledWith(addDocPendingChange(filename));
+  });
+
+  it('should mark a file as changed (.transcript file)', () => {
+    const filename = 'my-file.transcript';
+    const command = registry.getCommand(File.Changed);
+    command(filename);
+
+    expect(mockDispatch).toHaveBeenCalledWith(addDocPendingChange(filename));
+  });
+});
---
+++ packages/app/main/src/windowManager.spec.ts
@@ -0,0 +1,157 @@
+//
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+//
+// Microsoft Bot Framework: http://botframework.com
+//
+// Bot Framework Emulator Github:
+// https://github.com/Microsoft/BotFramwork-Emulator
+//
+// Copyright (c) Microsoft Corporation
+// All rights reserved.
+//
+// MIT License:
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+import { WindowManager } from './windowManager';
+import { rememberZoomLevel } from './state/actions/windowStateActions';
+
+const mockDispatch = jest.fn();
+const mockSettings = {
+  windowState: {
+    zoomLevel: 0,
+  },
+};
+jest.mock('./state/store', () => ({
+  dispatch: action => mockDispatch(action),
+  getSettings: () => mockSettings,
+}));
+
+const mockOn = jest.fn();
+jest.mock('electron', () => ({
+  ipcMain: {
+    on: (eventName, handler) => mockOn(eventName, handler),
+  },
+}));
+
+describe('WindowManager', () => {
+  let windowManager: WindowManager;
+  beforeEach(() => {
+    windowManager = new WindowManager();
+    mockDispatch.mockClear();
+  });
+
+  it('should add a main window', () => {
+    const window: any = {};
+    windowManager.addMainWindow(window);
+
+    expect((windowManager as any).mainWindow).toEqual(window);
+  });
+
+  it('should return whether or not it has a main window', () => {
+    (windowManager as any).mainWindow = {};
+
+    expect(windowManager.hasMainWindow()).toBe(true);
+  });
+
+  it('should return the main window', () => {
+    const mockWindow = {};
+    (windowManager as any).mainWindow = mockWindow;
+
+    expect(windowManager.getMainWindow()).toEqual(mockWindow);
+  });
+
+  it('should add a window', () => {
+    const mockWindow: any = {};
+    windowManager.add(mockWindow);
+
+    expect((windowManager as any).windows).toEqual([mockWindow]);
+  });
+
+  it('should remove a window', () => {
+    const mockWindow: any = {};
+    (windowManager as any).windows = [mockWindow];
+    windowManager.remove(mockWindow);
+
+    expect((windowManager as any).windows).toEqual([]);
+  });
+
+  it('should zoom in', () => {
+    const mockMainWindow: any = {
+      webContents: {
+        setZoomLevel: jest.fn(),
+      },
+    };
+    const mockWindow: any = {
+      webContents: {
+        setZoomLevel: jest.fn(),
+      },
+    };
+    windowManager.addMainWindow(mockMainWindow);
+    windowManager.add(mockWindow);
+    windowManager.zoomIn();
+
+    // 0 -> 1
+    expect(mockMainWindow.webContents.setZoomLevel).toHaveBeenCalledWith(1);
+    expect(mockWindow.webContents.setZoomLevel).toHaveBeenCalledWith(1);
+    expect(mockDispatch).toHaveBeenCalledWith(rememberZoomLevel({ zoomLevel: 1 }));
+  });
+
+  it('should zoom out', () => {
+    const mockMainWindow: any = {
+      webContents: {
+        setZoomLevel: jest.fn(),
+      },
+    };
+    const mockWindow: any = {
+      webContents: {
+        setZoomLevel: jest.fn(),
+      },
+    };
+    windowManager.addMainWindow(mockMainWindow);
+    windowManager.add(mockWindow);
+    windowManager.zoomOut();
+
+    // 0 -> -1
+    expect(mockMainWindow.webContents.setZoomLevel).toHaveBeenCalledWith(-1);
+    expect(mockWindow.webContents.setZoomLevel).toHaveBeenCalledWith(-1);
+    expect(mockDispatch).toHaveBeenCalledWith(rememberZoomLevel({ zoomLevel: -1 }));
+  });
+
+  it('should close all windows', () => {
+    const mockClose = jest.fn();
+    const mockWindow1: any = {
+      close: mockClose,
+    };
+    const mockWindow2: any = {
+      close: mockClose,
+    };
+    const mockMainWindow: any = {};
+    windowManager.addMainWindow(mockMainWindow);
+    windowManager.add(mockWindow1);
+    windowManager.add(mockWindow2);
+    windowManager.closeAll();
+
+    expect(mockClose).toHaveBeenCalledTimes(2);
+    expect((windowManager as any).mainWindow).toBe(undefined);
+    expect((windowManager as any).windows).toEqual([]);
+  });
+});
