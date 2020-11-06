fff964d1f1e195b64e8067efc8edc528c20d380b
Srinaath Ravichandran <srravich@microsoft.com>
2020-11-05 23:57:45 -0800

    Support for updated middleware contracts
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>

--- packages/app/client/src/index.tsx
+++ packages/app/client/src/index.tsx
@@ -44,6 +44,26 @@ import Main from './ui/shell/mainContainer';
 import { store } from './state/store';
 import './ui/styles/globals.scss';
 
+import 'core-js/features/array/find-index';
+import 'core-js/features/array/find';
+import 'core-js/features/array/from';
+import 'core-js/features/array/includes';
+import 'core-js/features/array/iterator';
+import 'core-js/features/dom-collections';
+import 'core-js/features/math/sign';
+import 'core-js/features/number/is-finite';
+import 'core-js/features/object/assign';
+import 'core-js/features/object/entries';
+import 'core-js/features/object/from-entries';
+import 'core-js/features/object/is';
+import 'core-js/features/object/values';
+import 'core-js/features/promise';
+import 'core-js/features/promise/finally';
+import 'core-js/features/set';
+import 'core-js/features/string/ends-with';
+import 'core-js/features/string/starts-with';
+import 'core-js/features/symbol';
+
 interceptError();
 interceptHyperlink();
 
