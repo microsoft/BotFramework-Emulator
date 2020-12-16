25655252bd9d231c5ced8800642bf61924d569cc
Srinaath Ravichandran <srravich@microsoft.com>
2020-12-16 15:42:58 -0800

    Increased wait time to 40seconds to spin ngrok process
    Signed-off-by: Srinaath Ravichandran <srravich@microsoft.com>

--- packages/app/main/src/ngrok.ts
+++ packages/app/main/src/ngrok.ts
@@ -176,18 +176,18 @@ export class NgrokInstance {
       return { inspectUrl: this.inspectUrl };
     }
     this.ngrokProcess = this.spawnNgrok(opts);
-    // If we do not receive a ready state from ngrok within 15 seconds, emit and reject
+    // If we do not receive a ready state from ngrok within 40 seconds, emit and reject
     this.inspectUrl = await new Promise<string>((resolve, reject) => {
       const timeout = setTimeout(() => {
-        const message = 'Failed to receive a ready state from ngrok within 15 seconds.';
+        const message = 'Failed to receive a ready state from ngrok within 40 seconds.';
         this.ngrokEmitter.emit('error', message);
         reject(message);
-      }, 15000);
+      }, 40000);
 
       /**
        * Look for an address in the many messages
        * sent by ngrok or fail if one does not arrive
-       * in 15 seconds.
+       * in 40 seconds.
        */
       const onNgrokData = (data: Buffer) => {
         const addr = data.toString().match(addrRegExp);
