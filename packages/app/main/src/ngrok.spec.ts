f4c97be82445c0f37d8ea1b13c3dce9946ec4ed4
Tony Anziano <tonyanziano5@gmail.com>
2021-02-11 10:51:00 -0800

    Fixed Mac build by updating `electron-builder` and `electron-updater` (#2230)
    * Bumped electron-builder to 22.9.1
    
    * Updated electron-updater to 4.3.5
    
    * Added changelog entry
    
    * Updated package lock
    
    * Fixed jest compilation error for one test.
    
    * More test fixes.

--- packages/app/main/src/ngrok.spec.ts
+++ packages/app/main/src/ngrok.spec.ts
@@ -60,6 +60,10 @@ jest.mock('./state', () => ({
   },
 }));
 
+jest.mock('fs-extra', () => ({}));
+
+jest.mock('@microsoft/bf-chatdown', () => ({}));
+
 const headersMap: Map<string, string> = new Map();
 headersMap.set('Server', 'Emulator');
 const tunnelResponseGeneric = (status: number, errorBody: string, headers = headersMap) => {
