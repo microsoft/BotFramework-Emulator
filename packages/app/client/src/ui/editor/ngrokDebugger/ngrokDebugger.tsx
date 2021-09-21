447a024bc7dce6051e93005b975a41466202d97c
Federico Bernal <64086728+FedericoBernal@users.noreply.github.com>
2021-09-21 14:16:31 -0300

    Fixed Ngrok status viewer heading

--- packages/app/client/src/ui/editor/ngrokDebugger/ngrokDebugger.tsx
+++ packages/app/client/src/ui/editor/ngrokDebugger/ngrokDebugger.tsx
@@ -32,7 +32,7 @@
 //
 
 import React from 'react';
-import { Column, Row, LinkButton, MediumHeader, SmallHeader } from '@bfemulator/ui-react';
+import { Column, Row, LinkButton, SmallHeader } from '@bfemulator/ui-react';
 import { TunnelCheckTimeInterval, TunnelError, TunnelStatus } from '@bfemulator/app-shared';
 
 import { GenericDocument } from '../../layout';
@@ -129,7 +129,10 @@ export const NgrokDebugger = (props: NgrokDebuggerProps) => {
 
   return (
     <GenericDocument className={styles.ngrokDebuggerContainer}>
-      <MediumHeader>Ngrok Status Viewer</MediumHeader>
+      &nbsp;
+      <h1>
+        <b>Ngrok Status Viewer</b>
+      </h1>
       <Row>
         <Column>
           <section>
